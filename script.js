c34fa7f567d6a1b0d1bb1f44e512de6dbfdd5f88
gabriel427131 <gabriel.d.oliveira1@hotmail.com>
2023-07-23 11:20:52 -0600

    Refactor script.js

--- script.js
+++ script.js
@@ -1,71 +1,69 @@
 'use strict';
 
-// console.log(document.querySelector('.message').textContent);
-// document.querySelector('.message').textContent = '🥳 Correct Number!';
-// console.log(document.querySelector('.message').textContent);
-
-// document.querySelector('.number').textContent = 13;
-// document.querySelector('.score').textContent = 10;
-
-// document.querySelector('.guess').value = 23;
-// console.log(document.querySelector('.guess').value);
-
 let secretNumber = Math.trunc(Math.random() * 20) + 1;
-
 let score = 20; // Is part of the state of the application.
+let highscore = 0;
 
+const displayMessage = function (message) {
+  document.querySelector('.message').textContent = message;
+};
+const displayNumber = function (number) {
+  document.querySelector('.number').textContent = number;
+};
+const changeBodyStyleBackColor = function (color) {
+  document.querySelector('body').style.backgroundColor = color;
+};
+const changeNumberStyleWidth = function (width) {
+  document.querySelector('.number').style.width = width;
+};
+const displayScore = function (score) {
+  document.querySelector('.score').textContent = score;
+};
+
+// Happens when 'Check!' button is clicked (resets everything except highscore)
 document.querySelector('.check').addEventListener('click', function () {
   const guess = Number(document.querySelector('.guess').value);
   console.log(guess, typeof guess);
 
   // When there is no input
   if (!guess) {
-    document.querySelector('.message').textContent = '🤔 No Number!';
+    displayMessage('🤔 No Number!');
 
     // When player wins
   } else if (guess === secretNumber) {
-    document.querySelector('.message').textContent = '🏆🥳 Correct Number!';
-    document.querySelector('.number').textContent = secretNumber;
+    displayMessage('🏆🥳 Correct Number!');
+    displayNumber(secretNumber);
+    changeBodyStyleBackColor('#60b347');
+    changeNumberStyleWidth('30rem');
 
-    document.querySelector('body').style.backgroundColor = '#60b347';
-
-    document.querySelector('.number').style.width = '30rem';
-
-    // When guess is too high
-  } else if (guess > secretNumber) {
-    if (score > 1) {
-      document.querySelector('.message').textContent = '⤴ Too   high!';
-      score--;
-      document.querySelector('.score').textContent = score;
-    } else {
-      document.querySelector('.message').textContent =
-        '❌😥 You lost the game!';
-      score--;
-      document.querySelector('.score').textContent = score;
+    // Sets highscore if score is greater
+    if (score > highscore) {
+      highscore = score;
+      document.querySelector('.highscore').textContent = highscore;
     }
 
-    // When guess is too low
-  } else if (guess < secretNumber) {
+    // Displays messagges if guess is different from secret number
+  } else if (guess !== secretNumber) {
     if (score > 1) {
-      document.querySelector('.message').textContent = '⤴ Too   low!';
+      displayMessage(guess > secretNumber ? '⤴ Too high!' : '⤵ Too low!');
       score--;
-      document.querySelector('.score').textContent = score;
+      displayScore(score);
     } else {
-      document.querySelector('.message').textContent =
-        '❌😥 You lost the game!';
+      displayMessage('❌😥 You lost the game!');
       score--;
-      document.querySelector('.score').textContent = score;
+      displayScore(0);
     }
   }
 });
 
+// Happens when 'Again!' button is clicked (resets everything except highscore)
 document.querySelector('.again').addEventListener('click', function () {
   secretNumber = Math.trunc(Math.random() * 20) + 1;
   score = 20;
-  document.querySelector('.score').textContent = score;
-  document.querySelector('.message').textContent = 'Start guessing...';
-  document.querySelector('.number').textContent = '?';
+  displayScore(score);
+  displayMessage('Start guessing...');
+  displayNumber('?');
   document.querySelector('.guess').value = '';
-  document.querySelector('body').style.backgroundColor = '#222';
-  document.querySelector('.number').style.width = '15rem';
+  changeBodyStyleBackColor('#222');
+  changeNumberStyleWidth('15rem');
 });
